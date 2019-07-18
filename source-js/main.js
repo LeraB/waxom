b1dc460c2c0eed689b214a4453465ff0dc93e803
vbriantseva <valeriia.briantseva@alphawebgroup.com>
2019-07-18 08:55:09 +0300

    init

---
+++ source-js/main.js
@@ -0,0 +1,52 @@
+var $filterBtn = document.getElementsByClassName('filter-btn');
+
+// Toggles the "active" class for the buttons.
+
+for (var i = 0; i < $filterBtn.length; i++) {
+
+  $filterBtn[i].addEventListener("click", function (event) {
+    document.querySelectorAll(".filter-btn").forEach(function (elem) {
+      if (elem.classList.contains('active')) {
+        elem.classList.remove("active");
+      }
+    });
+
+    event.classList.add("active");
+  });
+}
+
+function filterSelection(type) {
+
+  if (type === 'all') {
+    document.querySelectorAll(".filter-item").forEach(function (elem) {
+      elem.classList.remove('d-none');
+    });
+  }
+  else {
+    document.querySelectorAll(".filter-item").forEach(function (elem) {
+      elem.classList.add('d-none');
+
+      if ((elem.classList.contains(type)) && (elem.classList.contains('d-none'))) {
+        elem.classList.remove('d-none');
+      }
+    });
+  }
+}
+
+function smoothScroll(Element) {
+
+  Element = document.getElementById(Element);
+  var selectedPosX = 0;
+  var selectedPosY = 0;
+  while (Element != null) {
+    selectedPosX += Element.offsetLeft;
+    selectedPosY += Element.offsetTop;
+    Element = Element.offsetParent;
+  }
+  window.scrollTo(selectedPosX, selectedPosY);
+}
+
+document.getElementById('menuToggleInput').addEventListener("click", function (event) {
+  var $mainNavigation = document.getElementById('main-navigation');
+  $mainNavigation.classList.toggle('open');
+});
